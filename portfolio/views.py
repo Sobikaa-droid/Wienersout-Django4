553d0ec493ac25c56664eab83b4c1a387ba49f32
Alex Mack <olegbrain0@gmail.com>
2022-07-28 04:09:45 +0500

    Just First Test

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
 
 def home(request):
+    Project.objects()  # this is not an error, my Django support is not enabled in pycharm.
     return render(request, 'portfolio/home.html')
