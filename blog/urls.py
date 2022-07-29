8941921cd09e9e610d06f84eb596c3f997d10e92
Alex Mack <olegbrain0@gmail.com>
2022-07-29 15:48:38 +0500

    Shut Up

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views  # means import views from same folder where blog\urls is
 urlpatterns = [
     # this path below is 'localhost:8000/blog' because it set to 'blog/' in portfolio/views.py
     path('', views.all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from .models import Blog
 def all_blogs(request):
     blogs = Blog.objects.order_by('-date')[:5]  # order by most recent ones // limit to 5 post per page
     return render(request, 'blog/all_blogs.html', {'blogs': blogs, 'blogs_num': len(blogs)})
+
+
+def detail(request, blog_id):
+    return render(request, 'blog/detail.html')
